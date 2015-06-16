9710b60f0a00a113f6159ea9a66903cf4cafbdc2
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:45:22 +0300

    Update Shield.cs

--- YoukaiKingdom.Logic/Models/Items/Armors/Shield.cs
+++ YoukaiKingdom.Logic/Models/Items/Armors/Shield.cs
@@ -1,9 +1,13 @@
-﻿namespace YoukaiKingdom.Logic.Models.Items.Armors
+namespace YoukaiKingdom.Logic.Models.Items.Armors
 {
     using YoukaiKingdom.Logic.Interfaces;
 
     public class Shield : Armor, IOffhand
     {
+        private const int DefaultDefense = 100;
+        private const int DefaultLevel = 1;
         public Shield(string name, int level, int defensePoints) : base(name, level, defensePoints) { }
+        public Shield(string name) : base(name, DefaultLevel, DefaultDefense) { }
+       
     }
 }
