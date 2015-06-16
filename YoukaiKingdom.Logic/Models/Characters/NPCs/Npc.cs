c7ce131142c7b8b27d998621df8481a6e861a604
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 10:11:01 +0300

    Update Npc.cs

--- YoukaiKingdom.Logic/Models/Characters/NPCs/Npc.cs
+++ YoukaiKingdom.Logic/Models/Characters/NPCs/Npc.cs
@@ -1,5 +1,10 @@
-﻿namespace YoukaiKingdom.Logic.Models.Characters.NPCs
+namespace YoukaiKingdom.Logic.Models.Characters.NPCs
 {
+     using YoukaiKingdom.Logic.Interfaces;
+    using YoukaiKingdom.Logic.Models.Characters.NPCs;
+    using YoukaiKingdom.Logic.Models.Items.Weapons;
+    using YoukaiKingdom.Logic.Models.Characters.Heroes;
+    using YoukaiKingdom.Logic.Models.Items.Potions;
     /// <summary>
     /// Base class for non-player characters
     /// </summary>
@@ -7,7 +12,14 @@
     {
         protected Npc(string name, int health, int mana, int damage, int armor)
             : base(name, health, mana, damage, armor)
+        {}
+            
+        public virtual void RemoveHealthEffectsNPC(Hero damage)
         {
+            this.Health -= damage.Damage;
         }
+       
+        
     }
 }
+
