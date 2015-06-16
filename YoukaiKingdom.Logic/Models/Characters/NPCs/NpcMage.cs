31502cda722bcf524f828a14f15d37693ff56c3f
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 10:11:34 +0300

    Update NpcMage.cs

--- YoukaiKingdom.Logic/Models/Characters/NPCs/NpcMage.cs
+++ YoukaiKingdom.Logic/Models/Characters/NPCs/NpcMage.cs
@@ -1,11 +1,15 @@
-﻿namespace YoukaiKingdom.Logic.Models.Characters.NPCs
+namespace YoukaiKingdom.Logic.Models.Characters.NPCs
 {
     using YoukaiKingdom.Logic.Interfaces;
 
     public class NpcMage : Npc
     {
         public NpcMage(string name, int health, int mana, int damage, int armor) : base(name, health, mana, damage, armor) { }
-
+       
+        public override void RemoveHealthEffectsNPC(Heroes.Hero damage)
+        {
+            base.RemoveHealthEffectsNPC(damage);
+        }
         public override void Hit(ICharacter target)
         {
             throw new System.NotImplementedException();
