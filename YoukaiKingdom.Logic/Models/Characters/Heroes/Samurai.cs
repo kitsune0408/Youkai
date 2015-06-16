0ef5e9b3a28a06ac26c3e2fdba58504a1fda0d54
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:41:56 +0300

    Update Samurai.cs

--- YoukaiKingdom.Logic/Models/Characters/Heroes/Samurai.cs
+++ YoukaiKingdom.Logic/Models/Characters/Heroes/Samurai.cs
@@ -1,7 +1,10 @@
-﻿namespace YoukaiKingdom.Logic.Models.Characters.Heroes
+namespace YoukaiKingdom.Logic.Models.Characters.Heroes
 {
     using System;
-
+    using YoukaiKingdom.Logic.Models.Characters.Heroes;
+    using YoukaiKingdom.Logic.Models.Items.Weapons;
+    using YoukaiKingdom.Logic.Models.Items.Armors;
+    using YoukaiKingdom.Logic.Models.Items.Potions;
     using YoukaiKingdom.Logic.Interfaces;
    
     public class Samurai : Hero
@@ -18,6 +21,26 @@
         public override void Hit(ICharacter target)
         {
             throw new NotImplementedException();
+        }
+        public override void ApplyArmorEffects(Armor armor)
+        {
+            base.ApplyArmorEffects(armor);
+        }
+        public override void ApplyHealthEffects(HealingPotion health)
+        {
+            base.ApplyHealthEffects(health);
+        }
+        public override void ApplyManaEffects(ManaPotion mana)
+        {
+            base.ApplyManaEffects(mana);
+        }
+        public override void ApplyWeaponEffects(Weapon weapon)
+        {
+            base.ApplyWeaponEffects(weapon);
+        }
+        public override void RemoveHealthEffects(NPCs.Npc damage, Armor armor)
+        {
+            base.RemoveHealthEffects(damage, armor);
         }
     }
 }
