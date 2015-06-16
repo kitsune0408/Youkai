7986476d40117da4c1e73fd48a8336efa50a377a
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:44:27 +0300

    Update Helmet.cs

--- YoukaiKingdom.Logic/Models/Items/Armors/Helmet.cs
+++ YoukaiKingdom.Logic/Models/Items/Armors/Helmet.cs
@@ -1,7 +1,11 @@
-﻿namespace YoukaiKingdom.Logic.Models.Items.Armors
+namespace YoukaiKingdom.Logic.Models.Items.Armors
 {
     public class Helmet : Armor
     {
+        private const int DefaultDefense = 90;
+        private const int DefaultLevel = 1;
         public Helmet(string name, int level, int defensePoints) : base(name, level, defensePoints) { }
+        public Helmet(string name) : base(name, DefaultLevel, DefaultDefense) { }
+       
     }
 }
