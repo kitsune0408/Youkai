0eba9fc9fa07fe26d36c9f507cee39142067ac5e
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:43:22 +0300

    Update Boots.cs

--- YoukaiKingdom.Logic/Models/Items/Armors/Boots.cs
+++ YoukaiKingdom.Logic/Models/Items/Armors/Boots.cs
@@ -1,7 +1,12 @@
-﻿namespace YoukaiKingdom.Logic.Models.Items.Armors
+namespace YoukaiKingdom.Logic.Models.Items.Armors
 {
+
     public class Boots : Armor
     {
+        private const int DefaultDefense = 30;
+        private const int DefaultLevel = 1;
         public Boots(string name, int level, int defensePoints) : base(name, level, defensePoints) { }
+        public Boots(string name) : base(name, DefaultLevel, DefaultDefense) { }
+        
     }
 }
