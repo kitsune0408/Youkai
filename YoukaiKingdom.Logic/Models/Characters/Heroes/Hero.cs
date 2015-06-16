225ef99245800ad586a9e604d78af57bc0b53512
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:39:18 +0300

    Update Hero.cs

--- YoukaiKingdom.Logic/Models/Characters/Heroes/Hero.cs
+++ YoukaiKingdom.Logic/Models/Characters/Heroes/Hero.cs
@@ -1,8 +1,11 @@
-﻿namespace YoukaiKingdom.Logic.Models.Characters.Heroes
+namespace YoukaiKingdom.Logic.Models.Characters.Heroes
 {
     using YoukaiKingdom.Logic.Interfaces;
     using YoukaiKingdom.Logic.Models.Characters.NPCs;
     using YoukaiKingdom.Logic.Models.Inventar;
+    using YoukaiKingdom.Logic.Models.Items.Weapons;
+    using YoukaiKingdom.Logic.Models.Items.Armors;
+    using YoukaiKingdom.Logic.Models.Items.Potions;
 
     public abstract class Hero : Character
     {
@@ -11,7 +14,27 @@
         {
             this.Inventar = new Inventar();
         }
-
+        public virtual void ApplyWeaponEffects(Weapon weapon)
+        {
+            this.Damage = weapon.AttackPoints;
+        }
+        public virtual void ApplyArmorEffects(Armor armor)
+        {
+            this.Armor = armor.DefensePoints;
+        }
+        public virtual void ApplyManaEffects(ManaPotion mana)
+        {
+            this.Mana += mana.ManaPoints;
+        }
+        public virtual void ApplyHealthEffects(HealingPotion health)
+        {
+            this.Health += health.HealingPoints;
+        }
+        public virtual void RemoveHealthEffects(Npc damage, Armor armor)
+        {
+            this.Health -= damage.Damage;
+            this.Health += armor.DefensePoints;
+        }
         public override void Hit(ICharacter target)
         {
             if (target is Npc)
