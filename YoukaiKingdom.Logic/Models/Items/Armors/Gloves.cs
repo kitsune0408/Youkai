f5f629104881a8cdbffc162d25d4ba0936b0effe
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:44:01 +0300

    Update Gloves.cs

--- YoukaiKingdom.Logic/Models/Items/Armors/Gloves.cs
+++ YoukaiKingdom.Logic/Models/Items/Armors/Gloves.cs
@@ -1,7 +1,11 @@
-﻿namespace YoukaiKingdom.Logic.Models.Items.Armors
+namespace YoukaiKingdom.Logic.Models.Items.Armors
 {
     public class Gloves : Armor
     {
+        private const int DefaultDefense = 70;
+        private const int DefaultLevel = 1;
         public Gloves(string name, int level, int defensePoints) : base(name, level, defensePoints) { }
+        public Gloves(string name) : base(name, DefaultLevel, DefaultDefense) { }
+       
     }
 }
