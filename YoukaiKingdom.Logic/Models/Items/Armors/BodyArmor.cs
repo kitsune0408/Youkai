d97fe439da4674e9fd183afaf64591b453c87106
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:42:59 +0300

    Update BodyArmor.cs

--- YoukaiKingdom.Logic/Models/Items/Armors/BodyArmor.cs
+++ YoukaiKingdom.Logic/Models/Items/Armors/BodyArmor.cs
@@ -1,10 +1,20 @@
-﻿namespace YoukaiKingdom.Logic.Models.Items.Armors
+namespace YoukaiKingdom.Logic.Models.Items.Armors
 {
     public class BodyArmor : Armor
     {
+        private  const int DefaultDefense = 50;
+        private const int DefaultLevel = 1;
+
         public BodyArmor(string name, int level, int defensePoints)
             : base(name, level, defensePoints)
         {
         }
+        public BodyArmor(string name)
+            : base(name, DefaultLevel, DefaultDefense)
+        {
+        }
+        
+        
+        
     }
 }
