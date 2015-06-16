286b9ad83c0b2ab128b486b69e61e9c0d8e9d662
Stanimir-b <stanimir.b@gmail.com>
2015-06-16 09:47:20 +0300

    Update OneHandedSword.cs

--- YoukaiKingdom.Logic/Models/Items/Weapons/OneHandedSword.cs
+++ YoukaiKingdom.Logic/Models/Items/Weapons/OneHandedSword.cs
@@ -1,9 +1,14 @@
-﻿namespace YoukaiKingdom.Logic.Models.Items.Weapons
+namespace YoukaiKingdom.Logic.Models.Items.Weapons
 {
     using YoukaiKingdom.Logic.Interfaces;
 
     public class OneHandedSword : Weapon, IOffhand
     {
+        private const int DefaultAttackPoints = 70;
+        private const int DefaultLevel = 1;
+
         public OneHandedSword(string name, int level, int attackPoints) : base(name, level, attackPoints) { }
+        public OneHandedSword(string name) : base(name, DefaultLevel, DefaultAttackPoints) { }
+       
     }
 }
