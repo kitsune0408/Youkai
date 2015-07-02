2a8b619071929cb5d6a312fa1e24ffcab0490952
kitsune0408 <kitsune0408@yahoo.com>
2015-07-02 14:21:24 +0300

    class diagram and some fixes

--- YoukaiKingdom.Logic/Models/Characters/Spells/ТheЕqualizer.cs
+++ YoukaiKingdom.Logic/Models/Characters/Spells/ТheЕqualizer.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Timers;
-using YoukaiKingdom.Logic.Models.Characters.Heroes;
 namespace YoukaiKingdom.Logic.Models.Characters.Spells
 {
     class ТheЕqualizer : Spell
